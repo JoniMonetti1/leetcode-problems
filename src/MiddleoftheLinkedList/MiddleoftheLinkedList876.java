8c5e6a3c90c2f4c6f0cd95f238b293497d15288a
JoniMonetti1 <158229526+JoniMonetti1@users.noreply.github.com>
2024-09-11 17:05:35 -0300

    Problem 876 solved

---
+++ src/MiddleoftheLinkedList/MiddleoftheLinkedList876.java
@@ -0,0 +1,16 @@
+package MiddleoftheLinkedList;
+
+
+public class MiddleoftheLinkedList876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+
+        return slowPointer;
+    }
+}
